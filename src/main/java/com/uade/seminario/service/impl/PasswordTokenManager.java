a32829726c7dd4bdcca0f18fd1fefd3f57ca8417
Gonzalo <gonza.a.fuentes@gmail.com>
2017-06-05 00:44:33 -0300

    Primer Commit AppFuse

---
+++ src/main/java/com/uade/seminario/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package com.uade.seminario.service.impl;
+
+import com.uade.seminario.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
